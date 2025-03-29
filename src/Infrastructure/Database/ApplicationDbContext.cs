036c6b9d17571ad132f5f8db9d2866a78cc0f5a9
PGiokas <giokasp@cbs.gr>
2025-03-29 09:33:56 +0200

    Update DMS.Auth

---
+++ src/Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using DMS.Auth.Domain.Entities;
+using DMS.Auth.Application.Interfaces;
+
+namespace DMS.Auth.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public required DbSet<User> Users { get; set; }
+    public required DbSet<Role> Roles { get; set; }
+    public required DbSet<UserTotpSecret> UserTotpSecrets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configure User Entity
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("Users");
+            entity.HasKey(u => u.Id);
+
+            entity.Property(u => u.Username).HasMaxLength(200);
+            entity.Property(u => u.Email).HasMaxLength(300);                
+        });
+
+        //Configure User Entity
+        modelBuilder.Entity<UserTotpSecret>(entity =>
+        {
+            entity.ToTable("UserTotpSecrets");
+            entity.HasKey(u => u.Id);
+
+            entity.Property(x => x.UserId).IsRequired();
+            entity.Property(x => x.Base32Secret).IsRequired();
+            entity.HasIndex(x => x.UserId).IsUnique();
+        });
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
